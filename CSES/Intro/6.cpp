32b0df667b58a0ffa515196939a46f3425dcaba8
[invincible1786] <ruhaankakar1786.24@kgpian.iitkgp.ac.in>
2026-08-21 09:45:53 +0530

    spiral easy

---
+++ CSES/Intro/6.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long y, x;
+        cin >> y >> x;
+
+        if (x > y) {
+            if (x % 2 == 1) {
+                cout << x * x - y + 1;
+            } else {
+                cout << (x - 1) * (x - 1) + y;
+            }
+        } 
+        else {
+            if (y % 2 == 0) {
+                cout << y * y - x + 1;
+            } else {
+                cout << (y - 1) * (y - 1) + x;
+            }
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
