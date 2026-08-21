daccd12f9763d944d79542c4785f5174e8823de0
[invincible1786] <ruhaankakar1786.24@kgpian.iitkgp.ac.in>
2026-08-21 10:48:59 +0530

    zeroes in a factorial - simple number of total powers of 5, 25, ....

---
+++ CSES/Intro/9.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long ans = 0;
+
+    while (n > 0) {
+        n /= 5;
+        ans += n;
+    }
+
+    cout << ans << '\n';
+}
